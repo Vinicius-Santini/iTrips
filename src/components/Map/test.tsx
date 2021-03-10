a82837d6f9708607b1cfed39d6a506d1f436f512
Master <Master@Mac-mini-i5.local>
2021-03-10 15:18:01 -0300

    Implemented map first tests and logic

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -6,7 +6,34 @@ describe('<Map />', () => {
   it('should render without any marker', () => {
     render(<Map />)
 
-    screen.logTestingPlaygroundURL()
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument()
   })
-  // it('should render with the marker in correct place', () => {})
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Campinas',
+      slug: 'campinas',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+    const placeTwo = {
+      id: '2',
+      name: 'Mônaco',
+      slug: 'monaco',
+      location: {
+        latitude: 43,
+        longitude: 7
+      }
+    }
+    render(<Map places={[place, placeTwo]} />)
+
+    expect(screen.getByTitle(/campinas/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/mônaco/i)).toBeInTheDocument()
+  })
 })
